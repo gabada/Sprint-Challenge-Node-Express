75dc6fa7a39bfeedc6e8a6017ee4a79982cc1fb4
Gill Abada <gill@callifi.com>
2019-03-22 12:38:58 -0400

    add project action route.

--- projects/projectRoutes.js
+++ projects/projectRoutes.js
@@ -47,6 +47,26 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.get('/projectAction/:id', (req, res) => {
+  const { id } = req.params;
+  db.getProjectActions(id)
+
+    .then(project => {
+      console.log(project.length);
+      if (project.length > 1) {
+        res.status(200).json(project);
+      } else {
+        res.status(404).json({ error: 'prohect not found' });
+      }
+    })
+
+    .catch(err => {
+      res
+        .status(404)
+        .json({ error: 'The project with the specified ID does not exist.' });
+    });
+});
+
 router.put('/:id', (req, res) => {
   const { id } = req.params;
   const { name, description, completed } = req.body;
