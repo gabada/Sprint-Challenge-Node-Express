53c168323256657e130d49afd72a1b0cecf03190
Gill Abada <gill@callifi.com>
2019-03-22 11:14:01 -0400

    basic express server setup.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+const express = require('express');
+
+const port = 4000;
+const server = express();
+
+server.use(express.json());
+
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
