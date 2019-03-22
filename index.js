5169dc2636e3d2e82f49f1e57402cf9139c944df
Gill Abada <gill@callifi.com>
2019-03-22 11:27:48 -0400

    import project routes.

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 // code away!
 const express = require('express');
 
+const projectRoutes = require('./projects/projectRoutes.js');
+
 const port = 4000;
 const server = express();
 
 server.use(express.json());
+server.use('/api/projects', projectRoutes);
 
 server.listen(port, () => {
   console.log(`server listening on port ${port}`);
